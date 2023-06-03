326308190402ac1432a97af3c3b646c32fe932e9
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2023-06-03 07:09:57 +0600

    add server

---
+++ src/main/java/com/hotel/hoteladmin/communication/Server.java
@@ -0,0 +1,53 @@
+package com.hotel.hoteladmin.communication;
+
+import com.hotel.hoteladmin.DButils.DButils;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Server extends Thread{
+
+    public Server(Socket stk){
+        this.stk = stk;
+    }
+    Socket stk;
+
+    public void run(){
+        try {
+            DataInputStream din = new DataInputStream(stk.getInputStream());
+            DataOutputStream dout = new DataOutputStream(stk.getOutputStream());
+            String str = "";
+            String[] list;
+            //StringBuilder s1;
+            do{
+                str = (String) din.readUTF();
+                list = str.split(",");
+
+                switch (list[0]){
+                    case "logInRequest":
+                        System.out.println(Arrays.toString(list));
+                        System.out.println(DButils.getUserExistance(list[1],list[2]));
+                        if(DButils.getUserExistance(list[1],list[2])){
+                            str = "true";
+                        }else {
+                            str = "false";
+                        }
+                }
+                //s1 = new StringBuilder(str);
+                //s1.reverse();
+                //str = s1.toString();
+                dout.writeUTF(str);
+                dout.flush();
+//                if(str.equals("pots")){
+//                    System.out.println("Connection lost from one of the client");
+//                }
+            }while (true);
+        }catch (IOException e){}
+    }
+}
---
+++ src/main/java/com/hotel/hoteladmin/communication/StartServer.java
@@ -0,0 +1,29 @@
+package com.hotel.hoteladmin.communication;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class StartServer extends Thread{
+    public void run(){
+        ServerSocket ss = null;
+        try {
+            ss = new ServerSocket(1234);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Server rms;
+        do{
+            Socket stk = null;
+            try {
+                assert ss != null;
+                stk = ss.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            rms = new Server(stk);
+            rms.start();
+            System.out.println("One Client Connected");
+        }while (true);
+    }
+}
