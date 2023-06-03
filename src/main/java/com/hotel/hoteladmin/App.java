623f2f1a010da72a360c1be8a6a41abb621ed5c7
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2023-06-03 07:11:16 +0600

    create instance of StartServer at App.java

--- src/main/java/com/hotel/hoteladmin/App.java
+++ src/main/java/com/hotel/hoteladmin/App.java
@@ -1,11 +1,18 @@
 package com.hotel.hoteladmin;
 
+import com.hotel.hoteladmin.communication.Server;
+import com.hotel.hoteladmin.communication.StartServer;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class App extends Application {
     @Override
@@ -15,6 +22,17 @@ public class App extends Application {
         stage.setTitle("Log In");
         stage.setScene(scene);
         stage.show();
+        StartServer server = new StartServer();
+        server.start();
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                server.interrupt();
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
